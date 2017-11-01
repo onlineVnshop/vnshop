5ab0a9a9d94e0c6b83ca71a8bf35fa32f8d786d9
DreamLi <535789859@qq.com>
2017-11-02 01:12:26 +0800

    NavBread

--- client/src/components/NavBread.vue
+++ client/src/components/NavBread.vue
@@ -2,7 +2,7 @@
     <div class="nav-breadcrumb-wrap">
         <div class="container">
             <nav class="nav-breadcrumb">
-                <a href="/">首页</a>
+                <a href="/">home</a>
                 <slot></slot>
             </nav>
         </div>
