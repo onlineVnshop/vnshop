71cd9bd939d80892b53724ad0471d442e2c94dd4
DreamLi <535789859@qq.com>
2017-11-02 13:43:40 +0800

    init

--- client/src/components/NavBread.vue
+++ client/src/components/NavBread.vue
@@ -2,7 +2,7 @@
     <div class="nav-breadcrumb-wrap">
         <div class="container">
             <nav class="nav-breadcrumb">
-                <a href="/">stark1</a>
+                <a href="/">stark12</a>
                 <slot></slot>
             </nav>
         </div>
